b7236d0abb1e5f4b996e5e816a584ed9afafedd9
Igor Kazakov <kazaksecondarymail@gmail.com>
2024-06-16 23:53:59 +0700

    refactoring

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,30 +1,27 @@
-import { Box, CssBaseline, ThemeProvider } from '@mui/material';
-import theme from '../theme/theme';
-import '../fonts/Inter/inter.css';
-import { Outlet } from 'react-router-dom';
-import Header from '../../widgets/Header';
-import Footer from '../../widgets/Footer';
-import SignIn from '../../features/SignIn';
-import { useState } from 'react';
-import MainContent from "../../components/MainContent/MainContent.jsx";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "../theme/theme";
+import "../fonts/Inter/inter.css";
+import { Outlet } from "react-router-dom";
+import Header from "../../widgets/Header";
+import Footer from "../../widgets/Footer";
+import SignIn from "../../features/SignIn";
+import MainContent from "../routes/main/MainContent.jsx";
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Box sx={{ position: 'relative', maxWidth: '1920px' }}>
-        <Header isLoggedIn={isLoggedIn} />
-        <Box>
-          <Outlet />
-        </Box>
-        <MainContent />
-        <Footer />
-        {false ? <SignIn /> : ''}
-      </Box>
-    </ThemeProvider>
-  );
+ return (
+  <ThemeProvider theme={theme}>
+   <CssBaseline />
+   <Box sx={{ position: "relative", maxWidth: "1920px" }}>
+    <Header />
+    <Box>
+     <Outlet />
+    </Box>
+    <MainContent />
+    <Footer />
+    {false ? <SignIn /> : ""}
+   </Box>
+  </ThemeProvider>
+ );
 };
 
 export default App;
