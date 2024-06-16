2b69d13e3354e53cf075f3e604ab786edb13605b
Igor Kazakov <kazaksecondarymail@gmail.com>
2024-06-16 23:04:41 +0700

    main content
    Доделать слайдер.

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,25 +1,27 @@
-import { Box, CssBaseline, ThemeProvider } from '@mui/material';
-import theme from '../theme/theme';
-import '../fonts/Inter/inter.css';
-import { Outlet } from 'react-router-dom';
-import Header from '../../widgets/Header';
-import Footer from '../../widgets/Footer';
-import SignIn from '../../features/SignIn';
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "../theme/theme";
+import "../fonts/Inter/inter.css";
+import { Outlet } from "react-router-dom";
+import Header from "../../widgets/Header";
+import Footer from "../../widgets/Footer";
+import SignIn from "../../features/SignIn";
+import MainContent from "../../components/MainContent/MainContent.jsx";
 
 const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Box sx={{ position: 'relative', maxWidth: '1920px' }}>
-        <Header />
-        <Box>
-          <Outlet />
-        </Box>
-        <Footer />
-        {false ? <SignIn /> : ''}
-      </Box>
-    </ThemeProvider>
-  );
+ return (
+  <ThemeProvider theme={theme}>
+   <CssBaseline />
+   <Box sx={{ position: "relative", maxWidth: "1920px" }}>
+    <Header />
+    <Box>
+     <Outlet />
+    </Box>
+    <MainContent />
+    <Footer />
+    {false ? <SignIn /> : ""}
+   </Box>
+  </ThemeProvider>
+ );
 };
 
 export default App;
--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -1,66 +1,64 @@
-import { Box } from '@mui/material';
-import Logo from '../../shared/ui/Logo';
-import { logoSize } from './constants';
-import SearchInput from '../../shared/ui/SearchInput';
-import NavLink from '../../shared/ui/NavLink';
-import Button from '../../shared/ui/Button';
+import { Box } from "@mui/material";
+import Logo from "../../shared/ui/Logo";
+import { logoSize } from "./constants";
+import SearchInput from "../../shared/ui/SearchInput";
+import NavLink from "../../shared/ui/NavLink";
+import Button from "../../shared/ui/Button";
 
 const Header = () => {
-  return (
-    <Box sx={{ p: '28px 80px', display: 'flex', columnGap: '66px' }}>
-      <Box sx={{ display: 'flex', columnGap: '48px', width: '100%' }}>
-        <Logo width={logoSize.width} height={logoSize.height} />
-        <Box
-          sx={{
-            paddingBottom: '11px',
-            display: 'flex',
-            flexDirection: 'column',
-            rowGap: '28px',
-            maxWidth: '1150px',
-            width: '100%',
-          }}
-        >
-          <SearchInput />
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-            }}
-          >
-            <NavLink rout='/main' text='Главная' />
-            <NavLink rout='/catalog' text='Каталог' />
-            <NavLink rout='/appraisal' text='Честная оценка ' />
-            <NavLink rout='/pricing' text='Ценообразование' />
-            <NavLink rout='/consultation' text='Арт-консультация' />
-            <NavLink rout='/news' text='Новости' />
-          </Box>
-        </Box>
-      </Box>
-      <Box
-        sx={{ display: 'flex', alignItems: 'flex-start', columnGap: '12px' }}
-      >
-        <Button
-          text='Вход'
-          bgColor='white'
-          padding='16px 0'
-          fontSize='24px'
-          lineHeight='33.6px'
-          width='140px'
-          color='#252525'
-          borderColor='#757575'
-        />
-        <Button
-          text='Регистрация'
-          bgColor='#252525'
-          padding='16px 0'
-          fontSize='24px'
-          lineHeight='33.6px'
-          width='232px'
-          color='white'
-        />
-      </Box>
+ return (
+  <Box sx={{ p: "28px 80px", display: "flex", columnGap: "66px" }}>
+   <Box sx={{ display: "flex", columnGap: "48px", width: "100%" }}>
+    <Logo width={logoSize.width} height={logoSize.height} />
+    <Box
+     sx={{
+      paddingBottom: "11px",
+      display: "flex",
+      flexDirection: "column",
+      rowGap: "28px",
+      maxWidth: "1150px",
+      width: "100%",
+     }}
+    >
+     <SearchInput />
+     <Box
+      sx={{
+       display: "flex",
+       justifyContent: "space-between",
+      }}
+     >
+      <NavLink rout="/main" text="Главная" />
+      <NavLink rout="/catalog" text="Каталог" />
+      <NavLink rout="/appraisal" text="Честная оценка " />
+      <NavLink rout="/pricing" text="Ценообразование" />
+      <NavLink rout="/consultation" text="Арт-консультация" />
+      <NavLink rout="/news" text="Новости" />
+     </Box>
     </Box>
-  );
+   </Box>
+   <Box sx={{ display: "flex", alignItems: "flex-start", columnGap: "12px" }}>
+    <Button
+     text="Вход"
+     bgColor="white"
+     padding="16px 0"
+     fontSize="24px"
+     lineHeight="33.6px"
+     width="140px"
+     color="#252525"
+     borderColor="#757575"
+    />
+    <Button
+     text="Регистрация"
+     bgColor="#252525"
+     padding="16px 0"
+     fontSize="24px"
+     lineHeight="33.6px"
+     width="232px"
+     color="white"
+    />
+   </Box>
+  </Box>
+ );
 };
 
 export default Header;
