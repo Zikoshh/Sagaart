4d21ab98e370c3c56a62d6ab8d7495443c02f8b9
Igor Kazakov <kazaksecondarymail@gmail.com>
2024-06-22 21:48:08 +0700

    cart is done?

--- src/app/routes/cart/constants/data.ts
+++ src/app/routes/cart/constants/data.ts
@@ -3,11 +3,25 @@ import cartItem1 from '../assets/CartItem1.png'
 
 const cart = {
   title: 'Корзина',
-  imageTitle: '123',
-  imageSubTitle: '123',
+  imagePrice: '40 000 ₽',
+  imagePrice1: '90 000 ₽',
+  imageAuthor: 'Анастасия Рыкунова',
+  imageTitle: 'Цветы войны',
+  imageYear:'Год: 2024',
+  imageSize: 'Размер: 58х70 см',
+  imageMaterial: 'Материал: масло, холст',
+  imageStyle: 'Стиль: реализм',
+    imageAuthor1: 'Сами Мендиратта',
+  imageTitle1: 'Графитовый день',
+  imageYear1:'Год: 2024',
+  imageSize1: 'РРазмер: 80х80 см',
+  imageMaterial1: 'Материал: масло, холст',
+  imageStyle1: 'Стиль: абстракционизм',
   img: cartItem,
   img1: cartItem1,
   buttonText: 'оформить',
+  imagePriceTotal: ' Итого: ',
+  imagePriceTotalSum: ' 130 000₽ ',
 };
 
 const button = {
--- src/app/routes/cart/index.tsx
+++ src/app/routes/cart/index.tsx
@@ -15,32 +15,50 @@ const Cart = () => {
     <Box sx={styles.infoContainer}>
      <Typography sx={styles.title}>{cart.title}</Typography>
     </Box>
-    <Box component={"img"} src={cart.img} sx={styles.img} />
     <Box>
-     <Typography sx={styles.imageAuthor}>{cart.imageAuthor}</Typography>
-     <Typography sx={styles.imageTitle}>{cart.imageTitle}</Typography>
-     <Typography sx={styles.imageYear}>{cart.imageYear}</Typography>
-     <Typography sx={styles.imageSize}>{cart.imageSize}</Typography>
-     <Typography sx={styles.imageMaterial}>{cart.imageMaterial}</Typography>
-     <Typography sx={styles.imageStyle}>{cart.imageStyle}</Typography>
+     <Box sx={styles.imageSection}>
+      <Box component={"img"} src={cart.img} sx={styles.img} />
+      <Box sx={styles.imageInfo}>
+       <Typography sx={styles.imageAuthor}>{cart.imageAuthor}</Typography>
+       <Typography sx={styles.imageTitle}>{cart.imageTitle}</Typography>
+       <Typography sx={styles.imageYear}>{cart.imageYear}</Typography>
+       <Typography sx={styles.imageSize}>{cart.imageSize}</Typography>
+       <Typography sx={styles.imageMaterial}>{cart.imageMaterial}</Typography>
+       <Typography sx={styles.imageStyle}>{cart.imageStyle}</Typography>
+      </Box>
+      <Typography sx={styles.imagePrice}>{cart.imagePrice}</Typography>
+     </Box>
+     <Box sx={styles.imageSection}>
+      <Box component={"img"} src={cart.img1} sx={styles.img1} />
+      <Box sx={styles.imageInfo}>
+       <Typography sx={styles.imageAuthor1}>{cart.imageAuthor1}</Typography>
+       <Typography sx={styles.imageTitle1}>{cart.imageTitle1}</Typography>
+       <Typography sx={styles.imageYear1}>{cart.imageYear1}</Typography>
+       <Typography sx={styles.imageSize1}>{cart.imageSize1}</Typography>
+       <Typography sx={styles.imageMaterial1}>{cart.imageMaterial1}</Typography>
+       <Typography sx={styles.imageStyle1}>{cart.imageStyle1}</Typography>
+      </Box>
+      <Typography sx={styles.imagePrice}>{cart.imagePrice1}</Typography>
+     </Box>
     </Box>
-    <Box component={"img"} src={cart.img1} sx={styles.img1} />
-    <Typography sx={styles.imageAuthor}>{cart.imageAuthor}</Typography>
-    <Typography sx={styles.imageTitle}>{cart.imageTitle}</Typography>
-    <Typography sx={styles.imageYear}>{cart.imageYear}</Typography>
-    <Typography sx={styles.imageSize}>{cart.imageSize}</Typography>
-    <Typography sx={styles.imageMaterial}>{cart.imageMaterial}</Typography>
-    <Typography sx={styles.imageStyle}>{cart.imageStyle}</Typography>
    </Box>
-   <Button
-    text={cart.buttonText}
-    bgColor={button.bgColor}
-    padding={button.padding}
-    fontSize={button.fontSize}
-    lineHeight={button.lineHeight}
-    width={button.width}
-    color={button.color}
-   />
+   <Box sx={styles.imagePriceTotalPrice}>
+    <Typography sx={styles.imagePriceTotalSum}>
+     {cart.imagePriceTotalSum}
+    </Typography>
+    <Typography sx={styles.imagePriceTotal}>{cart.imagePriceTotal}</Typography>
+   </Box>
+   <Box sx={styles.buttonBuy}>
+    <Button
+     text={cart.buttonText}
+     bgColor={button.bgColor}
+     padding={button.padding}
+     fontSize={button.fontSize}
+     lineHeight={button.lineHeight}
+     width={button.width}
+     color={button.color}
+    />
+   </Box>
    <Box sx={styles.paginationContainer}>
     <Pagination
      sx={styles.pagination}
