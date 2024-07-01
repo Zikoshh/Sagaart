6432fe29de91e46783f7dc2383d07f6f102a2910
Igor Kazakov <kazaksecondarymail@gmail.com>
2024-07-01 23:58:26 +0700

    Update index.tsx

--- src/app/routes/main/index.tsx
+++ src/app/routes/main/index.tsx
@@ -1,258 +1,282 @@
-import { NavLink, Form } from "react-router-dom";
-import UploadIcon from "@mui/icons-material/Upload";
-import * as React from "react";
-import { useState } from "react";
-import Modal from "@mui/material/Modal";
+import { NavLink, Form } from 'react-router-dom';
+import UploadIcon from '@mui/icons-material/Upload';
+import * as React from 'react';
+import { useState } from 'react';
+import Modal from '@mui/material/Modal';
 import {
- Box,
- Typography,
- IconButton,
- TextField,
- styled,
- Button,
-} from "@mui/material";
+  Box,
+  Typography,
+  IconButton,
+  TextField,
+  styled,
+  Button,
+} from '@mui/material';
 import {
- subscriptionData,
- catalogData,
- subscriptionButton,
- catalogBuyButton,
- catalogSellButton,
- cardsData,
- logoSize,
- modalData,
- button,
-} from "./constants/data";
-import styles from "./constants/styles";
-import Logo from "../../../shared/ui/Logo";
-import CloseIcon from "./assets/close.svg?react";
-import subscriptionImg from "./assets/subscriptionImg.jpeg";
-import catalogImg from "./assets/catalogImg.png";
-import uploadedImage from "./assets/uploadedImage.png";
-import ButtonUI from "../../../shared/ui/Button";
+  subscriptionData,
+  catalogData,
+  subscriptionButton,
+  catalogBuyButton,
+  catalogSellButton,
+  cardsData,
+  logoSize,
+  modalData,
+  button,
+} from './constants/data';
+import styles from './constants/styles';
+import Logo from '../../../shared/ui/Logo';
+import CloseIcon from './assets/close.svg?react';
+import subscriptionImg from './assets/subscriptionImg.jpeg';
+import catalogImg from './assets/catalogImg.png';
+import uploadedImage from './assets/uploadedImage.png';
+import ButtonUI from '../../../shared/ui/Button';
 
-const VisuallyHiddenInput = styled("input")({
- clip: "rect(0 0 0 0)",
- clipPath: "inset(50%)",
- height: 1,
- overflow: "hidden",
- position: "absolute",
- bottom: 0,
- left: 0,
- whiteSpace: "nowrap",
- width: 1,
- color: "black",
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+  color: 'black',
 });
 
 const Main = () => {
- const [open, setOpen] = useState(false);
+  const [sellModalOpen, setSellModalOpen] = useState(false);
+  const [cardModalOpen, setCardModalOpen] = useState(false);
 
- const handleOpen = () => {
-  setOpen(true);
- };
+  const handleOpenSellModal = () => {
+    setSellModalOpen(true);
+  };
 
- const handleClose = () => {
-  setOpen(false);
- };
+  const handleCloseSellModal = () => {
+    setSellModalOpen(false);
+  };
 
- const handleOpenCard = () => {
-  setOpen(true);
- };
+  const handleOpenCardModal = () => {
+    setCardModalOpen(true);
+  };
 
- return (
-  <Box sx={styles.main}>
-   <Box sx={styles.container}>
-    <Box>
-     <Typography sx={styles.title}>{subscriptionData.title}</Typography>
-     <Typography sx={styles.subtitle}>{subscriptionData.subtitle}</Typography>
-     <ButtonUI
-      text={subscriptionButton.text}
-      bgColor={subscriptionButton.bgColor}
-      padding={subscriptionButton.padding}
-      fontSize={subscriptionButton.fontSize}
-      lineHeight={subscriptionButton.lineHeight}
-      width={subscriptionButton.width}
-      color={subscriptionButton.color}
-     />
-    </Box>
-    <Box
-     component={"img"}
-     src={subscriptionImg}
-     sx={styles.subscriptionImg}
-    ></Box>
-   </Box>
-   <Box sx={styles.container}>
-    <Box component={"img"} src={catalogImg} sx={styles.catalogImg}></Box>
-    <Box>
-     <Typography sx={styles.title}>{catalogData.title}</Typography>
-     <Typography sx={styles.subtitle}>{catalogData.subtitle}</Typography>
-     <Box sx={styles.buttonsContainer}>
-      <ButtonUI
-       text={catalogBuyButton.text}
-       bgColor={catalogBuyButton.bgColor}
-       padding={catalogBuyButton.padding}
-       fontSize={catalogBuyButton.fontSize}
-       lineHeight={catalogBuyButton.lineHeight}
-       width={catalogBuyButton.width}
-       color={catalogBuyButton.color}
-      />
-      <ButtonUI
-       onClick={handleOpen}
-       text={catalogSellButton.text}
-       bgColor={catalogSellButton.bgColor}
-       padding={catalogSellButton.padding}
-       fontSize={catalogSellButton.fontSize}
-       lineHeight={catalogSellButton.lineHeight}
-       width={catalogSellButton.width}
-       color={catalogSellButton.color}
-       borderColor={catalogSellButton.borderColor}
-      />
-     </Box>
-    </Box>
-   </Box>
-   <Box sx={styles.cardsContainer}>
-    {cardsData.map((card) => {
-     return (
-      <Box key={card.title} sx={styles.card}>
-       <NavLink to={card.rout}>
-        <Box component={"img"} src={card.img} sx={styles.cardImg}></Box>
-       </NavLink>
-       <Typography sx={styles.cardTitle}>{card.title}</Typography>
+  const handleCloseCardModal = () => {
+    setCardModalOpen(false);
+  };
+
+  return (
+    <Box sx={styles.main}>
+      <Box sx={styles.container}>
+        <Box>
+          <Typography sx={styles.title}>{subscriptionData.title}</Typography>
+          <Typography sx={styles.subtitle}>
+            {subscriptionData.subtitle}
+          </Typography>
+          <ButtonUI
+            text={subscriptionButton.text}
+            bgColor={subscriptionButton.bgColor}
+            padding={subscriptionButton.padding}
+            fontSize={subscriptionButton.fontSize}
+            lineHeight={subscriptionButton.lineHeight}
+            width={subscriptionButton.width}
+            color={subscriptionButton.color}
+          />
+        </Box>
+        <Box
+          component={'img'}
+          src={subscriptionImg}
+          sx={styles.subscriptionImg}
+        ></Box>
       </Box>
-     );
-    })}
-   </Box>
-   <SellModalWindow
-    open={open}
-    handleClose={handleClose}
-    handleOpenCard={handleOpenCard}
-   />
-  </Box>
- );
+      <Box sx={styles.container}>
+        <Box component={'img'} src={catalogImg} sx={styles.catalogImg}></Box>
+        <Box>
+          <Typography sx={styles.title}>{catalogData.title}</Typography>
+          <Typography sx={styles.subtitle}>{catalogData.subtitle}</Typography>
+          <Box sx={styles.buttonsContainer}>
+            <ButtonUI
+              text={catalogBuyButton.text}
+              bgColor={catalogBuyButton.bgColor}
+              padding={catalogBuyButton.padding}
+              fontSize={catalogBuyButton.fontSize}
+              lineHeight={catalogBuyButton.lineHeight}
+              width={catalogBuyButton.width}
+              color={catalogBuyButton.color}
+            />
+            <ButtonUI
+              onClick={handleOpenSellModal}
+              text={catalogSellButton.text}
+              bgColor={catalogSellButton.bgColor}
+              padding={catalogSellButton.padding}
+              fontSize={catalogSellButton.fontSize}
+              lineHeight={catalogSellButton.lineHeight}
+              width={catalogSellButton.width}
+              color={catalogSellButton.color}
+              borderColor={catalogSellButton.borderColor}
+            />
+          </Box>
+        </Box>
+      </Box>
+      <Box sx={styles.cardsContainer}>
+        {cardsData.map((card) => {
+          return (
+            <Box key={card.title} sx={styles.card}>
+              <NavLink to={card.rout}>
+                <Box component={'img'} src={card.img} sx={styles.cardImg}></Box>
+              </NavLink>
+              <Typography sx={styles.cardTitle}>{card.title}</Typography>
+            </Box>
+          );
+        })}
+      </Box>
+      <SellModalWindow
+        open={sellModalOpen}
+        handleClose={handleCloseSellModal}
+        handleOpenCard={handleOpenCardModal}
+        cardModalOpen={cardModalOpen}
+        handleCloseCardModal={handleCloseCardModal}
+      />
+    </Box>
+  );
 };
 
 function SellModalWindow({
- open,
- handleClose,
- handleOpenCard,
+  open,
+  handleClose,
+  handleOpenCard,
+  cardModalOpen,
+  handleCloseCardModal,
 }: {
- open: boolean;
- handleClose: () => void;
- handleOpenCard: () => void;
+  open: boolean;
+  handleClose: () => void;
+  handleOpenCard: () => void;
+  cardModalOpen: boolean;
+  handleCloseCardModal: () => void;
 }) {
- return (
-  <Modal open={open} onClose={handleClose}>
-   <Box sx={styles.SellModal}>
-    <Box sx={styles.SellModalHeader}>
-     <Logo width={logoSize.width} height={logoSize.height} />
-     <Typography sx={styles.SellInfoTitle}>{modalData.SellTitle}</Typography>
-     <IconButton onMouseDown={handleClose} sx={styles.closeButton}>
-      <CloseIcon />
-     </IconButton>
-    </Box>
-    <Form color="primary">
-     <Typography sx={styles.InputTitle}>{modalData.SellAuthorTitle}</Typography>
-     <TextField
-      sx={styles.SellArtInput}
-      inputProps={{ style: { fontSize: 24 } }}
-      placeholder="Имя фамилия"
-      required
-     ></TextField>
-     <Typography sx={styles.InputTitle}>{modalData.SellArtTitle}</Typography>
-     <TextField
-      sx={styles.SellArtInput}
-      inputProps={{ style: { fontSize: 24 } }}
-      required
-     ></TextField>
-     <Typography sx={styles.InputTitle}>
-      {modalData.SellArtDescriptionTitle}
-     </Typography>
-     <TextField
-      sx={styles.SellArtInput}
-      inputProps={{ style: { fontSize: 24 } }}
-      placeholder="Холст, масло ..."
-     ></TextField>
-     <Typography sx={styles.UploadTip}>{modalData.UploadTip}</Typography>
-     <Box>
-      <Box
-       component={"img"}
-       src={uploadedImage}
-       sx={styles.uploadedImage}
-      ></Box>
-      <Typography>{modalData.ImageName}</Typography>
-      <Typography>{modalData.ImageSize}</Typography>
-     </Box>
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handleOpenCard();
+    setTimeout(handleClose, 10);
+    handleClose();
+    console.log('123');
+  };
 
-     <Button
-      sx={{
-       backgroundColor: "white",
-       height: "50px",
-      }}
-      component="label"
-      role={undefined}
-      tabIndex={-1}
-      startIcon={
-       <UploadIcon
-        sx={{
-         color: "black",
-         backgroundColor: "white",
-         border: "1px solid black",
-         height: "30px",
-         width: "30px",
-        }}
-       />
-      }
-     >
-      <VisuallyHiddenInput type="file" />
-     </Button>
+  return (
+    <Modal open={open} onClose={handleClose}>
+      <Box sx={styles.SellModal}>
+        <Box sx={styles.SellModalHeader}>
+          <Logo width={logoSize.width} height={logoSize.height} />
+          <Typography sx={styles.SellInfoTitle}>
+            {modalData.SellTitle}
+          </Typography>
+          <IconButton onMouseDown={handleClose} sx={styles.closeButton}>
+            <CloseIcon />
+          </IconButton>
+        </Box>
+        <Form color="primary" onSubmit={handleSubmit}>
+          <Typography sx={styles.InputTitle}>
+            {modalData.SellAuthorTitle}
+          </Typography>
+          <TextField
+            sx={styles.SellArtInput}
+            inputProps={{ style: { fontSize: 24 } }}
+            placeholder="Имя фамилия"
+            required
+          ></TextField>
+          <Typography sx={styles.InputTitle}>
+            {modalData.SellArtTitle}
+          </Typography>
+          <TextField
+            sx={styles.SellArtInput}
+            inputProps={{ style: { fontSize: 24 } }}
+            required
+          ></TextField>
+          <Typography sx={styles.InputTitle}>
+            {modalData.SellArtDescriptionTitle}
+          </Typography>
+          <TextField
+            sx={styles.SellArtInput}
+            inputProps={{ style: { fontSize: 24 } }}
+            placeholder="Холст, масло ..."
+          ></TextField>
+          <Typography sx={styles.UploadTip}>{modalData.UploadTip}</Typography>
+          <Box>
+            <Box
+              component={'img'}
+              src={uploadedImage}
+              sx={styles.uploadedImage}
+            ></Box>
+            <Typography>{modalData.ImageName}</Typography>
+            <Typography>{modalData.ImageSize}</Typography>
+          </Box>
 
-     <Box sx={styles.SellPrice}>
-      <Typography align="right" sx={styles.SellPrice}>
-       {modalData.SellPrice}
-      </Typography>
-     </Box>
-     <ButtonUI
-      onClick={(handleOpenCard, handleClose)}
-      text={modalData.buttonText}
-      bgColor={button.bgColor}
-      padding={button.padding}
-      fontSize={styles.ButtonfontSize}
-      lineHeight={modalData.buttonlineHeight}
-      color={button.color}
-      width={styles.ButtonWidth}
-     />
-     <CardModal />
-    </Form>
-   </Box>
-  </Modal>
- );
+          <Button
+            sx={{
+              backgroundColor: 'white',
+              height: '50px',
+            }}
+            component="label"
+            role={undefined}
+            tabIndex={-1}
+            startIcon={
+              <UploadIcon
+                sx={{
+                  color: 'black',
+                  backgroundColor: 'white',
+                  border: '1px solid black',
+                  height: '30px',
+                  width: '30px',
+                }}
+              />
+            }
+          >
+            <VisuallyHiddenInput type="file" />
+          </Button>
+
+          <Box sx={styles.SellPrice}>
+            <Typography align="right" sx={styles.SellPrice}>
+              {modalData.SellPrice}
+            </Typography>
+          </Box>
+          <ButtonUI
+            type="submit"
+            text={modalData.buttonText}
+            bgColor={button.bgColor}
+            padding={button.padding}
+            fontSize={styles.ButtonfontSize}
+            lineHeight={modalData.buttonlineHeight}
+            color={button.color}
+            width={styles.ButtonWidth}
+          />
+        </Form>
+        <CardModal open={cardModalOpen} handleClose={handleCloseCardModal} />
+      </Box>
+    </Modal>
+  );
 }
 
-function CardModal() {
- const [open, setOpen] = React.useState(false);
- const handleOpenCard = () => {
-  setOpen(true);
- };
-
- const handleCloseCard = () => {
-  setOpen(false);
- };
-
- return (
-  <React.Fragment>
-   <Button onClick={handleOpenCard}>Модалка с картой</Button>
-   <Modal
-    open={open}
-    onClose={handleCloseCard}
-    aria-labelledby="child-modal-title"
-    aria-describedby="child-modal-description"
-   >
-    <Box sx={{ width: 200 }}>
-     <h2 id="child-modal-title">карта</h2>
-     <Button onClick={handleCloseCard}>закрыть</Button>
-    </Box>
-   </Modal>
-  </React.Fragment>
- );
+function CardModal({
+  open,
+  handleClose,
+}: {
+  open: boolean;
+  handleClose: () => void;
+}) {
+  return (
+    <React.Fragment>
+      <Button onClick={handleClose}>Открыть CardModal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="child-modal-title"
+        aria-describedby="child-modal-description"
+      >
+        <Box sx={{ width: 200 }}>
+          <h2 id="child-modal-title">карта</h2>
+          <Button onClick={handleClose}>закрыть</Button>
+        </Box>
+      </Modal>
+    </React.Fragment>
+  );
 }
 
 export default Main;
